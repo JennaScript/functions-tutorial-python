9445fdc505f4b1e84bc730bc5fdcab6d293bb798
Jennalyn Kabiling <67028701+JennaScript@users.noreply.github.com>
2020-09-08 07:15:31 -0700

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,41 @@
+#Functions Tutorial
+
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+print (result)
+
+result = area(10, 10)
+print (result)
+
+result = area(11, 12)
+print (result)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+result = subtract (100, 50)
+print (result)
+
+result = subtract (500, 200)
+print (result)
+
+def divide (num1, num2):
+    result = num1 / num2
+    return result
+    
+result = divide (40, 5)
+print (result)
+
+def add (value1, value2):
+    result = value1 + value2
+    return result
+
+result = add (100, 500)
+print (result)
+
+result = add (600, 100)
+print (result)
